d04db44d6d7cb3e1dd6f6bad6c9b6145c021d547
onmaxdt-devops <sithiraac@gmail.com>
2023-04-18 09:34:46 +0400

    Updated test files.

--- test/flutter_radio_player_test.dart
+++ test/flutter_radio_player_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_radio_player/flutter_radio_player.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_radio_player');
