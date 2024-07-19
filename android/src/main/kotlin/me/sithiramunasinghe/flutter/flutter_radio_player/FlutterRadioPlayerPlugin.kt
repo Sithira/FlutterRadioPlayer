47dcc7b9fa9af891e65fe9f08e3c4673565126e9
Sithira Munasinghe <sithiraac@gmail.com>
2024-07-19 10:38:01 +0530

    WIP

--- android/src/main/kotlin/me/sithiramunasinghe/flutter/flutter_radio_player/FlutterRadioPlayerPlugin.kt
+++ android/src/main/kotlin/me/sithiramunasinghe/flutter/flutter_radio_player/FlutterRadioPlayerPlugin.kt
@@ -1,37 +1,68 @@
 package me.sithiramunasinghe.flutter.flutter_radio_player
 
-import androidx.annotation.NonNull
-
+import android.content.ComponentName
+import android.content.Context
+import androidx.media3.common.MediaItem
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
+import com.google.common.util.concurrent.MoreExecutors
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import me.sithiramunasinghe.flutter.flutter_radio_player.core.PlaybackService
+
 
 /** FlutterRadioPlayerPlugin */
-class FlutterRadioPlayerPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class FlutterRadioPlayerPlugin : FlutterPlugin, MethodCallHandler {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
+    private var applicationContext: Context? = null
+    private var mediaController: MediaController? = null
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_radio_player")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_radio_player")
+        channel.setMethodCallHandler(this)
+        applicationContext = flutterPluginBinding.applicationContext
+        val token = SessionToken(
+            flutterPluginBinding.applicationContext, ComponentName(
+                flutterPluginBinding.applicationContext,
+                PlaybackService::class.java
+            )
+        )
+        val factory = MediaController.Builder(applicationContext!!, token)
+            .buildAsync()
+        factory.addListener({
+            mediaController = factory.let {
+                if (it.isDone) {
+                    it.get()
+                } else {
+                    null
+                }
+            }
+        }, MoreExecutors.directExecutor())
     }
-  }
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        if (call.method == "getPlatformVersion") {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else if (call.method == "play") {
+            val mediaItem: MediaItem = MediaItem.fromUri("https://radio.lotustechnologieslk.net:2020/stream/hirufmgarden")
+            mediaController!!.setMediaItem(mediaItem)
+            mediaController!!.prepare()
+            mediaController!!.play()
+        } else {
+            result.notImplemented()
+        }
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 }
 
 // https://github.com/oguzhaneksi/RadioRoam/tree/master
--- android/src/main/kotlin/me/sithiramunasinghe/flutter/flutter_radio_player/core/PlaybackService.kt
+++ android/src/main/kotlin/me/sithiramunasinghe/flutter/flutter_radio_player/core/PlaybackService.kt
@@ -1,18 +1,38 @@
 package me.sithiramunasinghe.flutter.flutter_radio_player.core
 
-import android.app.Activity
-import android.app.PendingIntent
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
-import androidx.media3.common.MediaItem
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.OptIn
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.media3.common.PlaybackException
 import androidx.media3.common.Player
+import androidx.media3.common.Player.STATE_IDLE
+import androidx.media3.common.Player.STATE_READY
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.HttpDataSource.HttpDataSourceException
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.CommandButton
+import androidx.media3.session.MediaNotification
 import androidx.media3.session.MediaSession
+import androidx.media3.session.MediaSession.Builder
 import androidx.media3.session.MediaSession.Callback
+import androidx.media3.session.MediaSession.ControllerInfo
 import androidx.media3.session.MediaSessionService
-import com.google.common.util.concurrent.Futures
-import com.google.common.util.concurrent.ListenableFuture
+import androidx.media3.session.MediaStyleNotificationHelper
+import com.google.common.collect.ImmutableList
+import me.sithiramunasinghe.flutter.flutter_radio_player.R
 
-class PlaybackService : MediaSessionService() {
+
+class PlaybackService : MediaSessionService(), Callback {
+    @RequiresApi(Build.VERSION_CODES.O)
+    var channel: NotificationChannel =
+        NotificationChannel("", "PennSkanvTicChannel", NotificationManager.IMPORTANCE_MAX)
+
     private lateinit var player: Player
     private var mediaSession: MediaSession? = null
 
@@ -21,16 +41,96 @@ class PlaybackService : MediaSessionService() {
         initializeSessionAndPlayer()
     }
 
-    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
+    override fun onDestroy() {
+        if (mediaSession != null) {
+            mediaSession?.run {
+                player.release()
+                mediaSession?.release()
+                release()
+                mediaSession = null
+            }
+        }
+        super.onDestroy()
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        val player = mediaSession?.player
+        if (player != null) {
+            if (!player.playWhenReady && player.mediaItemCount == 0) {
+                stopSelf()
+            }
+        }
+    }
+
+    override fun onGetSession(controllerInfo: ControllerInfo): MediaSession? {
         return mediaSession
     }
 
+    @OptIn(UnstableApi::class)
     private fun initializeSessionAndPlayer() {
         player = ExoPlayer.Builder(this).build()
-        mediaSession = MediaSession.Builder(this, player).setCallback(object : Callback {
+        mediaSession = Builder(this, player)
+            .setCallback(this)
+            .build()
+        player.addListener(object : Player.Listener {
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                if (isPlaying) {
+                    println("IsPlaying")
+                } else {
+                    println("IsStopped")
+                }
+            }
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                if (playbackState == STATE_IDLE) {
+                    println("STOPPEDDDDDDDDDD")
+                }
 
-        }).build()
+                if (playbackState == STATE_READY) {
+                    println("PLAYIIINGGGGGGGGG")
+                }
+            }
+
+            override fun onPlayerError(error: PlaybackException) {
+                val cause = error.cause
+                if (cause is HttpDataSourceException) {
+                    println("Oh no")
+                }
+            }
+        })
+        setMediaNotificationProvider(object : MediaNotification.Provider{
+            override fun createNotification(
+                mediaSession: MediaSession,
+                customLayout: ImmutableList<CommandButton>,
+                actionFactory: MediaNotification.ActionFactory,
+                onNotificationChangedCallback: MediaNotification.Provider.Callback
+            ): MediaNotification {
+                return updateNotification(mediaSession)
+            }
+
+            override fun handleCustomCommand(
+                session: MediaSession,
+                action: String,
+                extras: Bundle
+            ): Boolean {
+                return false
+            }
+
+        })
     }
 
+    @SuppressLint("UnsafeOptInUsageError")
+    private fun updateNotification(session: MediaSession): MediaNotification {
+
+        val notify = NotificationCompat.Builder(this,"fsdfdsfsdfs")
+            // This is globally changed every time when
+            // I add a new MediaItem from background service
+            .setContentTitle("Test")
+            .setContentText("Aaaa")
+            .setSmallIcon(R.drawable.media3_notification_small_icon)
+            .setStyle(MediaStyleNotificationHelper.MediaStyle(session))
+            .build()
+
+        return MediaNotification(9876, notify)
+    }
 
 }
